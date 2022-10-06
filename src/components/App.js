33ec529049852e7299b2c68f3b3e8a06ee68ee98
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-10-06 16:48:50 +0300

    created desktop version

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import cloudy from './assets/Cloud-background.png';
 import location from './assets/icons/location.svg';
 import shower from './assets/Shower.png';
+import pin from './assets/icons/pin.svg';
+import navigation from './assets/icons/navigation.svg';
 
 function App() {
+  const styleOfLine = {
+    position: 'absolute',
+    width: '50%',
+    height: '8px',
+    'background-color': '#FFEC65',
+    'border-radius': '80px',
+  };
+
   return (
     <div className="page">
       <div className="left-side">
@@ -24,15 +34,145 @@ function App() {
               <span className="left-side__temperature__icon">℃</span>
             </h2>
 
-            <h4>Shower</h4>
+            <h4 className="left-side__type-of-weather">Shower</h4>
 
             <div className="left-side__bottom">
-              <p>sadfsaf</p>
+              <span className="left-side__date">Today &nbsp; &middot; &nbsp; Fri, 5 Jun</span>
+
+              <p className="left-side__pin">
+                <img src={pin} alt="" className="left-side__pin__img" />
+                Helsinki
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <div className="right-side">ffff</div>
+      <div className="right-side">
+        <div className="wrapper wrapper--right-side">
+          <div className="right-side__content">
+            <div className="right-side__top">
+              <button className="right-side__button right-side__button--active">℃</button>
+              <button className="right-side__button">℉</button>
+            </div>
+
+            <div className="right-side__middle">
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+              <div className="weather-box">
+                <span>Tomorrow</span>
+
+                <img src={shower} alt="" className="weather-box__img" />
+
+                <div className="weather-box__temperature">
+                  <span>16°C</span>
+                  <span className="weather-box__night">11°C</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="right-side__bottom">
+              <h2 className="right-side__bottom__title">Today’s Hightlights </h2>
+
+              <div className="hightlight">
+                <div className="hightlight__container">
+                  <span className="hightlight__title">Wind status</span>
+                  <span className="hightlight__main">
+                    7<span className="hightlight__submain">mph</span>
+                  </span>
+                  <div className="hightlight__navigation">
+                    <div className="hightlight__container-img">
+                      <img src={navigation} alt="" className="hightlight__img" />
+                    </div>
+
+                    <span className="hightlight__subtitle">WSW</span>
+                  </div>
+                </div>
+
+                <div className="hightlight__container">
+                  <span className="hightlight__title">Humidity</span>
+                  <span className="hightlight__main hightlight__main--humidity">
+                    84<span className="hightlight__submain">%</span>
+                  </span>
+
+                  <div className="hightlight__number">
+                    <span>0</span>
+                    <span>50</span>
+                    <span>100</span>
+                  </div>
+
+                  <div className="hightlight__line">
+                    <div style={styleOfLine}></div>
+                  </div>
+
+                  <span className="hightlight__percent">%</span>
+                </div>
+
+                <div className="hightlight__container">
+                  <span className="hightlight__title">Visibility</span>
+                  <span className="hightlight__main hightlight__main--humidity">
+                    6,4 <span className="hightlight__submain">miles</span>
+                  </span>
+                </div>
+
+                <div className="hightlight__container">
+                  <span className="hightlight__title">Air Pressure</span>
+                  <span className="hightlight__main hightlight__main--humidity">
+                    988 <span className="hightlight__submain">mb</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
